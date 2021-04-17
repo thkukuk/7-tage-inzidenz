77a580c73cca1d75f5f4ed569dd77102422bc5c1
Thorsten Kukuk <kukuk@suse.com>
2021-04-17 09:41:26 +0200

    Enhance build script

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+	echo "Usage: build.sh <TAG>"
+	exit 1
+fi
+
 sudo podman build -t 7-tage-inzidenz .
+sudo podman login docker.io
+sudo podman tag localhost/7-tage-inzidenz thkukuk/7-tage-inzidenz:$1
+sudo podman tag localhost/7-tage-inzidenz thkukuk/7-tage-inzidenz:latest
+sudo podman push thkukuk/7-tage-inzidenz:$1
+sudo podman push thkukuk/7-tage-inzidenz:latest
